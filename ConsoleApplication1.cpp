9d4fc24f7ae37bf8aa179160b6671eebcd07a21c
Karol Kułaga <33664932+EscPL@users.noreply.github.com>
2022-05-23 13:16:04 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    if (number == 2 || number == 1) {
+        cout << "NIE";
+        return 0;
+    }
+    
+    for (int i = 1; i <= number; i+=2) {
+        cout << i << " ";
+    }
+    for (int i = 0; i <= number; i += 2) {
+        cout << i << " ";
+    }
+}
