b5a266069cb3024ecb653156f101b3e5baacafb0
Karol Kułaga <33664932+EscPL@users.noreply.github.com>
2022-05-23 13:17:56 +0200

    Update and rename ConsoleApplication1.cpp to Niekolejne.cpp

--- Niekolejne.cpp
+++ Niekolejne.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -11,7 +11,7 @@ int main()
         return 0;
     }
     
-    for (int i = 1; i <= number; i+=2) {
+    for (int i = 1; i <= number; i += 2) {
         cout << i << " ";
     }
     for (int i = 0; i <= number; i += 2) {
