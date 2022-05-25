8ec434e6b6eb080c2063726f04dc62479db468b4
Karol Kułaga <33664932+EscPL@users.noreply.github.com>
2022-05-25 14:15:55 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void check_collinearity(float Ax, float Ay, float Bx, float By, float Cx, float Cy) {
+    if (Ax == Bx || Ax == Cx || Bx == Cx) {
+        if (Ax == Bx && Ax == Cx) {
+            cout << "TAK" << endl;
+            return;
+        }
+        else {
+            cout << "NIE" << endl;
+            return;
+        }
+    }
+    float a1, a2, a3;
+    a1 = (Ay - By) / (Ax - Bx);
+    a2 = (Ay - Cy) / (Ax - Cx);
+    a3 = (By - Cy) / (Bx - Cx);
+
+    if (a1 == a2 && a1 == a3) cout << "TAK" << endl;
+    else cout << "NIE" << endl;
+}
+
+int main()
+{
+    int test;
+    cin >> test;
+
+    while (test--) {
+        float Ax, Ay, Bx, By, Cx, Cy;
+        cin >> Ax >> Ay >> Bx >> By >> Cx >> Cy;
+
+        check_collinearity(Ax, Ay, Bx, By, Cx, Cy);
+    }
+    return 0;
+}
