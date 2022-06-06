7e5af75c2077fbcba69abef913ac9e2d7913c7e4
Karol Kułaga <33664932+EscPL@users.noreply.github.com>
2022-06-06 17:27:05 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void move_numbers(int numbers[], int temp[], int how_many, int cycles) {
+    int counter = 0;
+    for (int i = 0; i < how_many; i++) {
+        if (how_many - i > cycles) numbers[i] = temp[i + cycles];
+        else {
+            numbers[i] = temp[counter];
+            counter++;
+        }
+    }
+}
+
+int main()
+{
+    int how_many, cycles;
+    cin >> how_many >> cycles;
+    int* numbers = new int[how_many];
+    int* temp = new int[how_many];
+
+    for (int i = 0; i < how_many; i++) {
+        cin >> numbers[i];
+        temp[i] = numbers[i];
+    }
+    
+    move_numbers(numbers, temp, how_many, cycles);
+
+    for (int i = 0; i < how_many; i++) {
+        cout << numbers[i] << " ";
+    }
+
+    delete[] numbers;
+    delete[] temp;
+
+    return 0;
+}
