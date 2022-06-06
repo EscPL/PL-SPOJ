c907079f77a60f2cf367b87b808c124b74ee8c89
Karol Kułaga <33664932+EscPL@users.noreply.github.com>
2022-06-06 14:54:11 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test;
+    cin >> test;
+
+    while (test--) {
+        char pesel[11];
+        int digits[11];
+
+        for (int i = 0; i < 11; i++) {
+            cin >> pesel[i];
+            digits[i] = (int)pesel[i] - 48;
+        }
+
+        int sum = 0;
+        sum += 1 * (digits[0] + digits[4] + digits[8] + digits[10]);
+        sum += 3 * (digits[1] + digits[5] + digits[9]);
+        sum += 7 * (digits[2] + digits[6]);
+        sum += 9 * (digits[3] + digits[7]);
+
+        if (sum % 10 == 0) cout << "D" << endl;
+        else cout << "N" << endl;
+    }
+    return 0;
+}
